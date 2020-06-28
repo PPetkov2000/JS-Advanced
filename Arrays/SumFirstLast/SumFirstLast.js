3ae77c2f2243967aa24fceea421ff9c840cf0a16
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-06-28 16:55:19 +0300

    remove unnecessary files

--- Arrays/SumFirstLast/SumFirstLast.js
+++
@@ -1,9 +0,0 @@
-function solve(arr) {
-  return Number(arr[0]) + Number(arr[arr.length - 1]);
-}
-
-console.log(solve(["20", "30", "40"]));
-
-console.log("_".repeat(40) + "\n");
-
-console.log(solve(["5", "10"]));
